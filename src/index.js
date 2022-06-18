d55842aa9c6d48672004790ee0d88938fa71ff18
Shanol <102300466+shanolhere@users.noreply.github.com>
2022-06-18 18:40:17 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,29 @@
+// const convertTemp = () => {
+//   const number = document.querySelector("#num").value;
+//   const temp = document.querySelector("#temp").value;
+//   const final = document.querySelector(".final");
+//   // console.log(number);
+//   // console.log(temp);
+
+//   const convertCelToFah = (celsius) => {
+//     let fahrenheit;
+//     fahrenheit = (celsius * 5) / 9 + 32;
+//     return fahrenheit;
+//   };
+
+//   const convertFahToCel = (fahrenheit) => {
+//     let celsius;
+//     celsius = ((fahrenheit - 32) * 9) / 5;
+//     return celsius;
+//   };
+
+//   let result;
+
+//   if (temp === "Celsius") {
+//     result = convertCelToFah(number);
+//     final.innerHTML(`= ${result} °Faherenheit`);
+//   } else {
+//     result = convertFahToCel(number);
+//     final.innerHTML(`= ${result} °Celsius`);
+//   }
+// };
